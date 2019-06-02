61d8bb11bcd9f82f455d8aefb085c21746cd114e
QAEng2827 <inna.nakopyuk@gmail.com>
2019-06-02 12:31:36 +0300

    create brunch dev

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(".gitignore word!");
         System.out.println(".gitignore git!");
+        System.out.println("hi");
 	// write your code here
     }
 }
