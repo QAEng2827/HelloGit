093f401cd65d0c3073c3df991732f291a124acae
QAEng2827 <inna.nakopyuk@gmail.com>
2019-06-02 12:33:17 +0300

    changed Hello

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(".gitignore word!");
         System.out.println(".gitignore git!");
-	// write your code here
+        System.out.println("ejkeljelkj");
     }
 }
