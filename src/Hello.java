d6439d57ed88abf2cb5a1b2f199ebc81d5990868
QAEng2827 <inna.nakopyuk@gmail.com>
2019-06-02 12:16:06 +0300

    ignore

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,9 @@
 public class Hello {
-   public st
-
+    public static void main(String[] args) {
+       // System.out.println(".gitignore word!");
+        System.out.println(".gitignore git!");
+        // write your code here
+    }
 }
+
+
